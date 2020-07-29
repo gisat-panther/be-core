b84efacc00ee14f848814aabefaf9105b51b487e
Miloslav Nenadal <miloslav.nenadal@imatic.cz>
2020-07-29 09:06:06 +0200

    Allow audit for all types

---
+++ migrations/202007290846.undo.sql
@@ -0,0 +1,52 @@
+DROP TRIGGER audit_trigger_row ON "application"."application";
+DROP TRIGGER audit_trigger_stm ON "application"."application";
+DROP TRIGGER audit_trigger_row ON "application"."configuration";
+DROP TRIGGER audit_trigger_stm ON "application"."configuration";
+DROP TRIGGER audit_trigger_row ON "application"."layerTree";
+DROP TRIGGER audit_trigger_stm ON "application"."layerTree";
+
+DROP TRIGGER audit_trigger_row ON "dataSources"."attributeDataSource";
+DROP TRIGGER audit_trigger_stm ON "dataSources"."attributeDataSource";
+DROP TRIGGER audit_trigger_row ON "dataSources"."dataSource";
+DROP TRIGGER audit_trigger_stm ON "dataSources"."dataSource";
+DROP TRIGGER audit_trigger_row ON "dataSources"."raster";
+DROP TRIGGER audit_trigger_stm ON "dataSources"."raster";
+DROP TRIGGER audit_trigger_row ON "dataSources"."vector";
+DROP TRIGGER audit_trigger_stm ON "dataSources"."vector";
+DROP TRIGGER audit_trigger_row ON "dataSources"."wms";
+DROP TRIGGER audit_trigger_stm ON "dataSources"."wms";
+DROP TRIGGER audit_trigger_row ON "dataSources"."wmts";
+DROP TRIGGER audit_trigger_stm ON "dataSources"."wmts";
+
+DROP TRIGGER audit_trigger_row ON "metadata"."areaTree";
+DROP TRIGGER audit_trigger_stm ON "metadata"."areaTree";
+DROP TRIGGER audit_trigger_row ON "metadata"."areaTreeLevel";
+DROP TRIGGER audit_trigger_stm ON "metadata"."areaTreeLevel";
+DROP TRIGGER audit_trigger_row ON "metadata"."attribute";
+DROP TRIGGER audit_trigger_stm ON "metadata"."attribute";
+DROP TRIGGER audit_trigger_row ON "metadata"."attributeSet";
+DROP TRIGGER audit_trigger_stm ON "metadata"."attributeSet";
+DROP TRIGGER audit_trigger_row ON "metadata"."case";
+DROP TRIGGER audit_trigger_stm ON "metadata"."case";
+DROP TRIGGER audit_trigger_row ON "metadata"."layerTemplate";
+DROP TRIGGER audit_trigger_stm ON "metadata"."layerTemplate";
+DROP TRIGGER audit_trigger_row ON "metadata"."period";
+DROP TRIGGER audit_trigger_stm ON "metadata"."period";
+DROP TRIGGER audit_trigger_row ON "metadata"."place";
+DROP TRIGGER audit_trigger_stm ON "metadata"."place";
+DROP TRIGGER audit_trigger_row ON "metadata"."scenario";
+DROP TRIGGER audit_trigger_stm ON "metadata"."scenario";
+DROP TRIGGER audit_trigger_row ON "metadata"."scope";
+DROP TRIGGER audit_trigger_stm ON "metadata"."scope";
+DROP TRIGGER audit_trigger_row ON "metadata"."style";
+DROP TRIGGER audit_trigger_stm ON "metadata"."style";
+DROP TRIGGER audit_trigger_row ON "metadata"."tag";
+DROP TRIGGER audit_trigger_stm ON "metadata"."tag";
+
+DROP TRIGGER audit_trigger_row ON "specific"."esponFuoreIndicator";
+DROP TRIGGER audit_trigger_stm ON "specific"."esponFuoreIndicator";
+DROP TRIGGER audit_trigger_row ON "specific"."lpisChangeCase";
+DROP TRIGGER audit_trigger_stm ON "specific"."lpisChangeCase";
+
+DROP TRIGGER audit_trigger_row ON "views"."view";
+DROP TRIGGER audit_trigger_stm ON "views"."view";
