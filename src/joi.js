8e442fc41e7f28e5047295c317cbab504c34acb9
Miloslav Nenadal <miloslav.nenadal@imatic.cz>
2020-10-15 15:04:00 +0200

    Add isoDuration validation

--- src/joi.js
+++ src/joi.js
@@ -1,11 +1,27 @@
 const Joi = require('@hapi/joi');
+const momentInterval = require('moment-interval');
 
-module.exports = Joi.extend((joi) => ({
-    type: 'stringArray', // array items are encoded in string, separated by comma
-    base: Joi.array().meta({baseType: 'array'}),
-    coerce: (value) => {
-        if (value != null && value.split) {
-            return {value: value.split(',')};
-        }
-    },
-}));
+const extensions = [
+    (joi) => ({
+        type: 'stringArray', // array items are encoded in string, separated by comma
+        base: Joi.array().meta({baseType: 'array'}),
+        coerce: (value) => {
+            if (value != null && value.split) {
+                return {value: value.split(',')};
+            }
+        },
+    }),
+    (joi) => ({
+        type: 'isoDuration',
+        base: Joi.string().meta({baseType: 'string'}),
+        validate: (value, helpers) => {
+            const {start, end} = momentInterval.interval(value);
+
+            if (!start().isValid() || !end().isValid()) {
+                return {value, errors: helpers.error('isoDuration.interval')};
+            }
+        },
+    }),
+];
+
+module.exports = extensions.reduce((joi, ext) => joi.extend(ext), Joi);
