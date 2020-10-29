bb84efaff32d6719e67be40fd972f4b160a1a6f8
Miloslav Nenadal <miloslav.nenadal@imatic.cz>
2020-10-29 15:37:04 +0100

    Audit translations

--- migrations/202010221206.do.sql
+++ migrations/202010221206.do.sql
@@ -7,3 +7,5 @@ CREATE TABLE "public"."translations"(
   "value" JSONB,
   PRIMARY KEY("resourceKey", "resourceGroup", "resourceType", "locale", "field")
 );
+
+SELECT audit.audit_table('"public"."translations"');
