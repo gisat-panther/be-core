49c66042ebcd566bf4bec487550e1dbad9d3a524
Martin Babič <martin.babic@gisat.cz>
2021-04-05 14:13:38 +0200

    [WIP] tiled import

--- src/applications/data/src/processors/import.js
+++ src/applications/data/src/processors/import.js
@@ -4,6 +4,7 @@ const fs = require('fs');
 const path = require('path');
 const _ = require('lodash');
 const chp = require('child_process');
+const ptrTileGrid = require('@gisatcz/ptr-tile-grid');
 
 const config = require('../../../../../config');
 
@@ -26,7 +27,7 @@ const extractPackage = (file, importKey) => {
 	return Promise
 		.resolve()
 		.then(() => {
-			let zipFs = new admZip(file.buffer);
+			let zipFs = new admZip(_.isObject(file) ? file.buffer : file);
 			zipFs.extractAllTo(`${basePath}${importKey}`, true);
 		})
 		.then(() => {
@@ -67,7 +68,7 @@ const importSpatialDataToPostgres = (importKey, path) => {
 	let {host, user, password, database} = config.pgConfig.normal;
 	return new Promise((resolve, reject) => {
 		chp.exec(
-			`ogr2ogr -f "PostgreSQL" "PG:host=${host} user=${user} password=${password} dbname=${database}" -nlt PROMOTE_TO_MULTI -lco GEOMETRY_NAME=geom -lco LAUNDER=NO ${path}`,
+			`ogr2ogr -f "PostgreSQL" "PG:host=${host} user=${user} password=${password} dbname=${database}" -nlt PROMOTE_TO_MULTI -lco SPATIAL_INDEX=GIST -lco GEOMETRY_NAME=geom -lco LAUNDER=NO ${path}`,
 			(error, stdout, stderr) => {
 				if (error) {
 					reject(error);
@@ -77,7 +78,7 @@ const importSpatialDataToPostgres = (importKey, path) => {
 					resolve();
 				}
 			});
-	});
+	})
 }
 
 const getExistingLayers = (layerNames) => {
@@ -147,6 +148,14 @@ const processShapefile = (importKey, name, files, options) => {
 					})
 			}
 		})
+		.then(() => {
+			if (options && options.tiled) {
+				return createTilesForLayer(name, options)
+					.then(() => {
+						log(importKey, `tiles for ${name} calculated`)
+					})
+			}
+		})
 }
 
 const clearLayerData = (layerName) => {
@@ -159,6 +168,11 @@ const clearLayerData = (layerName) => {
 		})
 		.then(() => {
 			return db.query(`DROP TABLE "${layerName}" CASCADE`)
+				.catch(() => {
+				})
+		})
+		.then(() => {
+			return db.query(`DROP TABLE "${layerName}_tiles" CASCADE`)
 				.catch(() => {
 				})
 		})
@@ -201,6 +215,47 @@ const createTopologyForLayer = (layerName, options) => {
 						throw new Error(_.map(pgResult.rows, 'error').join(", "));
 					}
 				})
+		})
+}
+
+const createTilesForLayer = (layerName, options) => {
+	if (!options.fidColumnName) {
+		throw new Error(`missing fidColumnName options`);
+	}
+
+
+	return db
+		.query(
+			`ALTER TABLE "${layerName}" ADD CONSTRAINT "unique_${options.fidColumnName}" UNIQUE ("${options.fidColumnName}")`
+		)
+		.then(() => {
+			return db
+				.query(
+					`SELECT "data_type" AS "fidColumnType" FROM "information_schema"."columns" WHERE "table_name" = '${layerName}' AND "column_name" = '${options.fidColumnName}'`
+				)
+		})
+		.then((pgResult) => {
+			return pgResult.rows[0].fidColumnType;
+		})
+		.then((fidColumnType) => {
+			return db
+				.query(
+					`CREATE TABLE "${layerName}_tiles" ("${options.fidColumnName}" ${fidColumnType} REFERENCES "${layerName}" ("${options.fidColumnName}"), "tile" TEXT, "level" INT)`
+				)
+		})
+		.then(() => {
+			return db
+				.query(
+					`SELECT ST_XMin(ST_Extent(geom)) AS "xMin", ST_YMin(ST_Extent(geom)) AS "yMin", ST_XMax(ST_Extent(geom)) AS "xMax", ST_YMax(ST_Extent(geom)) AS "yMax" FROM "${layerName}"`
+				)
+				.then((pgResult) => {
+					return [[pgResult.rows[0].xMin, pgResult.rows[0].yMin], [pgResult.rows[0].xMax, pgResult.rows[0].yMax]]
+				})
+		})
+		.then((bBox) => {
+			for (let level = 0; level <= 6; level++) {
+				console.log(JSON.stringify(ptrTileGrid.grid.getGridForLevelAndExtent(level, bBox)))
+			}
 		})
 }
 
@@ -277,12 +332,20 @@ const importFile = (file, user, options) => {
 
 	cache.set(`import_${importKey}`, {status: "running"})
 		.then(() => {
-			if (file.mimetype !== "application/zip") {
-				throw new Error("unsupported package type, zip file expected")
+			if (
+				file
+				&& file.mimetype === "application/zip"
+			) {
+				return extractPackage(file, importKey);
+			} else if (
+				options.fs
+				&& fs.existsSync(options.fs)
+				&& chp.execSync(`file --mime-type -b "${options.fs}"`).toString().trim() === "application/zip"
+			) {
+				return extractPackage(options.fs, importKey)
+			} else {
+				throw new Error("File not found or has unsupported type!")
 			}
-		})
-		.then(() => {
-			return extractPackage(file, importKey);
 		})
 		.then(() => {
 			return getFiles(importKey)
