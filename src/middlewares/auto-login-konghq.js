fb062feecb48b1260f2de033ef05abb3a61e0a1c
Martin Babič <martin.babic@gisat.cz>
2021-09-14 18:24:37 +0200

    auto login based on KongHQ header middleware

---
+++ src/middlewares/auto-login-konghq.js
@@ -0,0 +1,44 @@
+const auth = require('../modules/login/auth');
+const uuid = require('../uuid');
+const q = require('../modules/login/query');
+const info = require('../modules/login/info');
+const plan = require('../applications/plan');
+
+const UserType = {
+    USER: 'user'
+};
+
+/**
+ * If no user is logged in, logs in user by X-User-Info header provided by KongHQ.
+ */
+async function autoLoginKongHqMiddleware(request, response, next) {
+    if (request.user != null) {
+        return next();
+    }
+
+    const kongUserKey = request.headers['x-user-info'];
+
+    if (!uuid.isValid(kongUserKey)) {
+        return next();
+    }
+
+    if (!await q.getUserInfoByKey(kongUserKey)) {
+        return next();
+    }
+
+    const user = {
+        key: kongUserKey,
+        type: UserType.USER,
+        realKey: kongUserKey
+    };
+
+    const token = await auth.createAuthToken(
+        auth.tokenPayload(user)
+    );
+
+    request.user = user;
+    request.authToken = token;
+    next();
+}
+
+module.exports = autoLoginKongHqMiddleware;
