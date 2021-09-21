bab6fba170f29d0d6bf987e5b2bb4ded822cc40c
Martin Babič <martin.babic@gisat.cz>
2021-09-21 19:24:17 +0200

    worldCereal user methods CUD init

---
+++ src/applications/worldCereal/user/index.js
@@ -0,0 +1,17 @@
+function create(request, response) {
+    response.status(501).end();
+}
+
+function update(request, response) {
+    response.status(501).end();
+}
+
+function remove(request, response) {
+    response.status(501).end();
+}
+
+module.exports = {
+    create,
+    update,
+    remove
+}
