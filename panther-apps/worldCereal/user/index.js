d31dd4c706e0a931c9b2a03aa3524551715eec87
Martin Babič <martin.babic@gisat.cz>
2021-09-21 22:41:46 +0200

    worldCereal user create/update/remove methods

--- panther-apps/worldCereal/user/index.js
+++ panther-apps/worldCereal/user/index.js
@@ -1,13 +1,76 @@
+const result = require('../../../src/modules/rest/result');
+const handler = require('../../../src/modules/rest/handler');
+
 function create(request, response) {
-    response.status(501).end();
+    return handler
+        .create('user', {
+            user: request.user,
+            body: {
+                data: {
+                    users: [request.body]
+                }
+            }
+        })
+        .then((r) => {
+            if (r.type === result.CREATED) {
+                response.status(201).end();
+            } else if (r.type === result.FORBIDDEN) {
+                response.status(403).end();
+            } else {
+                response.status(500).end();
+            }
+        })
+        .catch((error) => {
+            response.status(500).end();
+        })
 }
 
 function update(request, response) {
-    response.status(501).end();
+    return handler
+        .update('user', {
+            user: request.user,
+            body: {
+                data: {
+                    users: [request.body]
+                }
+            }
+        })
+        .then((r) => {
+            if (r.type === result.UPDATED) {
+                response.status(200).end();
+            } else if (r.type === result.FORBIDDEN) {
+                response.status(403).end();
+            } else {
+                response.status(500).end();
+            }
+        })
+        .catch((error) => {
+            response.status(500).end();
+        })
 }
 
 function remove(request, response) {
-    response.status(501).end();
+    return handler
+        .deleteRecords('user', {
+            user: request.user,
+            body: {
+                data: {
+                    users: [request.body]
+                }
+            }
+        })
+        .then((r) => {
+            if (r.type === result.DELETED) {
+                response.status(200).end();
+            } else if (r.type === result.FORBIDDEN) {
+                response.status(403).end();
+            } else {
+                response.status(500).end();
+            }
+        })
+        .catch((error) => {
+            response.status(500).end();
+        })
 }
 
 module.exports = {
