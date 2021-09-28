c8ec507eee14289d4ef9b05c8dd76b6e14955e95
Martin Babič <martin.babic@gisat.cz>
2021-09-28 23:58:04 +0200

    allow set worldCereal product public on create/update & return product full description only on first time in user session

--- panther-apps/worldCereal/product/index.js
+++ panther-apps/worldCereal/product/index.js
@@ -1,10 +1,28 @@
 const uuidByString = require('uuid-by-string');
+const _ = require('lodash');
 
 const result = require('../../../src/modules/rest/result');
 const handler = require('../../../src/modules/rest/handler');
+const shared = require('../shared');
+const db = require('../../../src/db');
+
+const GROUPS = {
+    worldCerealPublic: "2dbc2120-b826-4649-939b-fff5a4a01866",
+    worldCerealUser: "2597df23-94d9-41e0-91f3-7ea633ae27f2"
+}
 
 function getKeyByProductId(productMetadata) {
     return uuidByString(productMetadata.id)
+}
+
+function setAsPublic(productKey) {
+    return db
+        .query(`SELECT "key" FROM "user"."permissions" WHERE "permission" = 'view' AND "resourceKey" = '${productKey}'`)
+        .then((pgResult) => pgResult.rows[0].key)
+        .then((permissionKey) => {
+            return db
+                .query(`INSERT INTO "user"."groupPermissions" ("groupKey", "permissionKey") VALUES ('${GROUPS.worldCerealPublic}', '${permissionKey}') ON CONFLICT DO NOTHING;`)
+        })
 }
 
 function create(request, response) {
@@ -17,6 +35,8 @@ function create(request, response) {
                         {
                             key: getKeyByProductId(request.body),
                             data: {
+                                tileKeys: request.body.tiles.map((tile) => tile.tile),
+                                bbox: request.body.geometry,
                                 data: request.body
                             }
                         }
@@ -26,15 +46,31 @@ function create(request, response) {
         })
         .then((r) => {
             if (r.type === result.CREATED) {
-                response.status(201).end();
+                return r.data.data.worldCerealProductMetadata[0];
             } else if (r.type === result.FORBIDDEN) {
-                response.status(403).end();
+                throw new Error(403);
+            } else {
+                throw new Error(500);
+            }
+        })
+        .then((worldCerealProductMetadata) => {
+            if (!worldCerealProductMetadata) {
+                throw new Error(500);
+            }
+
+            if (worldCerealProductMetadata.data.data.public) {
+                return setAsPublic(worldCerealProductMetadata.key);
+            }
+        })
+        .then(() => {
+            response.status(201).end();
+        })
+        .catch((error) => {
+            if (_.isNumber(Number(error.message))) {
+                response.status(error.message).end();
             } else {
                 response.status(500).end();
             }
-        })
-        .catch((error) => {
-            response.status(500).end();
         })
 }
 
@@ -48,6 +84,8 @@ function update(request, response) {
                         {
                             key: getKeyByProductId(request.body),
                             data: {
+                                tileKeys: request.body.tiles.map((tile) => tile.tile),
+                                bbox: request.body.geometry,
                                 data: request.body
                             }
                         }
@@ -57,15 +95,31 @@ function update(request, response) {
         })
         .then((r) => {
             if (r.type === result.UPDATED) {
-                response.status(200).end();
+                return r.data.data.worldCerealProductMetadata[0];
             } else if (r.type === result.FORBIDDEN) {
-                response.status(403).end();
+                throw new Error(403);
+            } else {
+                throw new Error(500);
+            }
+        })
+        .then((worldCerealProductMetadata) => {
+            if (!worldCerealProductMetadata) {
+                throw new Error(500);
+            }
+
+            if (worldCerealProductMetadata.data.data.public) {
+                return setAsPublic(worldCerealProductMetadata.key);
+            }
+        })
+        .then(() => {
+            response.status(200).end();
+        })
+        .catch((error) => {
+            if (_.isNumber(Number(error.message))) {
+                response.status(error.message).end();
             } else {
                 response.status(500).end();
             }
-        })
-        .catch((error) => {
-            response.status(500).end();
         })
 }
 
@@ -106,12 +160,42 @@ function view(request, response) {
                 filter: request.body
             }
         })
-        .then((r) => {
+        .then(async (r) => {
             if (r.type === result.SUCCESS) {
-                response.status(200).send(
-                    r.data.data.worldCerealProductMetadata.map(
-                        (product) => Object.assign({}, product, { permissions: undefined })
-                    )
+                const sharedStorageKey = `${request.user.realKey}_products`;
+
+                let sentProductKeys = await shared.get(sharedStorageKey) || [];
+
+                let products = r.data.data.worldCerealProductMetadata.map((product) => {
+                    if (!sentProductKeys.includes(product.key)) {
+                        sentProductKeys.push(product.key);
+
+                        return Object.assign(
+                            {},
+                            product,
+                            {
+                                permissions: undefined
+                            }
+                        );
+                    } else {
+                        return Object.assign(
+                            {},
+                            product,
+                            {
+                                data: undefined,
+                                permissions: undefined
+                            }
+                        );
+                    }
+                });
+
+                await shared.set(sharedStorageKey, sentProductKeys);
+
+                response.status(200).send({
+                    products,
+                    tiles: []
+                }
+
                 );
             } else if (r.type === result.FORBIDDEN) {
                 response.status(403).end();
@@ -128,5 +212,6 @@ module.exports = {
     create,
     update,
     remove,
-    view
+    view,
+    getKeyByProductId
 }
