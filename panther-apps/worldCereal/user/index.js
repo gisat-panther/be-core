dba65afb25d500e75d474afa738551b1a9300b37
Martin Babič <martin.babic@gisat.cz>
2021-09-28 23:56:18 +0200

    add worldCereal user to correct groups on create & allow start user session

--- panther-apps/worldCereal/user/index.js
+++ panther-apps/worldCereal/user/index.js
@@ -1,5 +1,14 @@
+const _ = require('lodash');
+
 const result = require('../../../src/modules/rest/result');
 const handler = require('../../../src/modules/rest/handler');
+const shared = require('../shared');
+const db = require('../../../src/db');
+
+const GROUPS = {
+    worldCerealPublic: "2dbc2120-b826-4649-939b-fff5a4a01866",
+    worldCerealUser: "2597df23-94d9-41e0-91f3-7ea633ae27f2"
+};
 
 function create(request, response) {
     return handler
@@ -13,15 +22,31 @@ function create(request, response) {
         })
         .then((r) => {
             if (r.type === result.CREATED) {
-                response.status(201).end();
+                return r.data.data.users[0].key;
             } else if (r.type === result.FORBIDDEN) {
-                response.status(403).end();
+                throw new Error(403);
+            } else {
+                throw new Error(500);
+            }
+        })
+        .then((userKey) => {
+            return db
+                .query(`
+                    BEGIN;
+                    INSERT INTO "user"."userGroups" ("userKey", "groupKey") VALUES ('${userKey}', '${GROUPS.worldCerealPublic}') ON CONFLICT DO NOTHING;
+                    INSERT INTO "user"."userGroups" ("userKey", "groupKey") VALUES ('${userKey}', '${GROUPS.worldCerealUser}') ON CONFLICT DO NOTHING;
+                    COMMIT;
+                `)
+        })
+        .then(() => {
+            response.status(201).end();
+        })
+        .catch((error) => {
+            if (_.isNumber(Number(error.message))) {
+                response.status(error.message).end();
             } else {
                 response.status(500).end();
             }
-        })
-        .catch((error) => {
-            response.status(500).end();
         })
 }
 
@@ -73,8 +98,17 @@ function remove(request, response) {
         })
 }
 
+function sessionStart(request, response) {
+    return shared
+        .set(`${request.user.realKey}_products`, [])
+        .then(() => {
+            response.status(200).end();
+        })
+}
+
 module.exports = {
     create,
     update,
-    remove
+    remove,
+    sessionStart
 }
