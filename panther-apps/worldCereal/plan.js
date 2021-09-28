a0fa45a9c805b4789bf6a6795c9c9436af9403db
Martin Babič <martin.babic@gisat.cz>
2021-09-28 23:51:55 +0200

    add new columns to plan

--- panther-apps/worldCereal/plan.js
+++ panther-apps/worldCereal/plan.js
@@ -1,5 +1,7 @@
 const Joi = require('joi');
 const uuid = require('../../src/uuid');
+const qb = require('@imatic/pgqb');
+const {SQL} = require('sql-template-strings');
 
 module.exports = {
     specific: {
@@ -8,18 +10,24 @@ module.exports = {
                 list: {
                     columns: [
                         'key',
+                        'tileKeys',
+                        'bbox',
                         'data'
                     ],
                 },
                 create: {
                     columns: [
                         'key',
+                        'tileKeys',
+                        'bbox',
                         'data'
                     ],
                 },
                 update: {
                     columns: [
                         'key',
+                        'tileKeys',
+                        'bbox',
                         'data'
                     ],
                 },
@@ -28,6 +36,24 @@ module.exports = {
                 key: {
                     defaultValue: () => uuid.generate(),
                     schema: Joi.string().uuid()
+                },
+                tileKeys: {
+                    defaultValue: null,
+                    schema: Joi.array().items(Joi.string()).allow(null)
+                },
+                bbox: {
+                    defaultValue: null,
+                    schema: Joi.object().allow(null),
+                    selectExpr: function ({alias}) {
+                        return qb.val.raw(`ST_AsGeoJSON(${alias}."bbox")::JSON AS "bbox"`);
+                    },
+                    modifyExpr: function ({value}) {
+                        if (value == null) {
+                            return qb.val.inlineParam(null);
+                        }
+
+                        return qb.val.raw(SQL`ST_GeomFromGeoJSON(${value})`);
+                    },
                 },
                 data: {
                     defaultValue: null,
