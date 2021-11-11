6f68bd3dde40fb2eb04b5a36a889dc0d70eec792
Martin Babič <martin.babic@gisat.cz>
2021-11-11 16:59:48 +0100

    add permissions #build

--- panther-apps/worldCereal/fixtures.sql
+++ panther-apps/worldCereal/fixtures.sql
@@ -26,7 +26,9 @@ INSERT INTO "user"."permissions"
     ("key", "resourceKey", "resourceGroup", "resourceType", "permission")
 VALUES
     ('b809c988-cfff-427d-9572-1e40a2fe92fe', null, 'specific', 'worldCerealProductMetadata', 'create'),
+    ('a405e8cf-13e0-4d9a-b039-91e4f8854b07', null, 'specific', 'worldCerealProductMetadata', 'view'),
     ('fd5aba3a-c8f8-493c-a304-a75e19c64df6', null, 'specific', 'worldCerealProductMetadata', 'update'),
+    ('647a7004-2a62-445d-a78d-43ac8b1bdb6e', null, 'specific', 'worldCerealProductMetadata', 'delete'),
     ('6897b1fc-a3e3-4195-a41a-f492d4a9df2a', null, 'user', 'users', 'create')
 ON CONFLICT DO NOTHING;
 
@@ -34,8 +36,12 @@ INSERT INTO "user"."groupPermissions"
     ("groupKey", "permissionKey")
 VALUES
     ('998c5760-e42e-457f-aa6d-247af6352c73', 'b809c988-cfff-427d-9572-1e40a2fe92fe'),
+    ('998c5760-e42e-457f-aa6d-247af6352c73', 'a405e8cf-13e0-4d9a-b039-91e4f8854b07'),
     ('998c5760-e42e-457f-aa6d-247af6352c73', 'fd5aba3a-c8f8-493c-a304-a75e19c64df6'),
+    ('998c5760-e42e-457f-aa6d-247af6352c73', '647a7004-2a62-445d-a78d-43ac8b1bdb6e'),
+
     ('998c5760-e42e-457f-aa6d-247af6352c73', '6897b1fc-a3e3-4195-a41a-f492d4a9df2a'),
+
     ('2597df23-94d9-41e0-91f3-7ea633ae27f2', 'b809c988-cfff-427d-9572-1e40a2fe92fe'),
     ('2597df23-94d9-41e0-91f3-7ea633ae27f2', 'fd5aba3a-c8f8-493c-a304-a75e19c64df6')
 ON CONFLICT DO NOTHING;
