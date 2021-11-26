6ebb92e153bfcc5a54597f6ecc53aa7ff8c8478a
Martin Babic <martin.babic@gisat.cz>
2021-11-26 20:04:10 +0100

    add worldcereal users and permissions

--- panther-apps/worldCereal/fixtures.sql
+++ panther-apps/worldCereal/fixtures.sql
@@ -2,7 +2,15 @@ INSERT INTO "user"."users"
     ("key", "email", "password", "phone", "name")
 VALUES
     ('3fdd158d-4b78-4d11-92c7-403b4adab4d8', 'worldCerealAdmin@example.com', null, null, null),
-    ('4fc1d704-b2e5-4fb7-839a-5baf3ad494ee', 'worldCerealUser@example.com', null, null, null)
+    ('4fc1d704-b2e5-4fb7-839a-5baf3ad494ee', 'worldCerealUser@example.com', null, null, null),
+    ('1e70bb3c-3575-406d-8d1d-d64359ec0027', 'vito_user', null, null, null),
+    ('ba621c03-bc65-4669-8df9-fc621143a99f', 'gisat_user', null, null, null),
+    ('441fb13d-abb6-4775-a732-907e5e5f0e58', 'iiasa_user', null, null, null),
+    ('3c8f1575-3a32-497e-9a77-0a75f0bcf86d', 'cs_user', null, null, null),
+    ('cbbf5f25-684d-4236-bd33-613f21a16dac', 'esa_user', null, null, null),
+    ('5db9613d-f8da-4167-bde6-11f9d84df775', 'cs_admin', null, null, null),
+    ('8c676118-a49e-4817-8b3f-edc742fdf43c', 'demo_rdm', null, null, null),
+    ('a7365eb7-e986-4660-8eed-3dd3a6350024', 'ewoc_internal', null, null, null)
 ON CONFLICT DO NOTHING;
 
 INSERT INTO "user"."groups"
@@ -16,10 +24,47 @@ ON CONFLICT DO NOTHING;
 INSERT INTO "user"."userGroups"
     ("userKey", "groupKey")
 VALUES
+    -- user: worldCerealAdmin@example.com | group: worldCerealAdmin
     ('3fdd158d-4b78-4d11-92c7-403b4adab4d8', '998c5760-e42e-457f-aa6d-247af6352c73'),
-    ('4fc1d704-b2e5-4fb7-839a-5baf3ad494ee', '2597df23-94d9-41e0-91f3-7ea633ae27f2'),
+    -- user: worldCerealAdmin@example.com | group: worldCerealPublic
     ('3fdd158d-4b78-4d11-92c7-403b4adab4d8', '2dbc2120-b826-4649-939b-fff5a4a01866'),
-    ('4fc1d704-b2e5-4fb7-839a-5baf3ad494ee', '2dbc2120-b826-4649-939b-fff5a4a01866')
+    -- user: worldCerealUser@example.com | group: worldCerealUser
+    ('4fc1d704-b2e5-4fb7-839a-5baf3ad494ee', '2597df23-94d9-41e0-91f3-7ea633ae27f2'),
+    -- user: worldCerealUser@example.com | group: worldCerealPublic
+    ('4fc1d704-b2e5-4fb7-839a-5baf3ad494ee', '2dbc2120-b826-4649-939b-fff5a4a01866'),
+    
+    -- user: vito_user | group: worldCerealUser
+    ('1e70bb3c-3575-406d-8d1d-d64359ec0027', '2597df23-94d9-41e0-91f3-7ea633ae27f2'),
+    -- user: vito_user | group: worldCerealPublic
+    ('1e70bb3c-3575-406d-8d1d-d64359ec0027', '2dbc2120-b826-4649-939b-fff5a4a01866'),
+    -- user: gisat_user | group: worldCerealUser
+    ('ba621c03-bc65-4669-8df9-fc621143a99f', '2597df23-94d9-41e0-91f3-7ea633ae27f2'),
+    -- user: gisat_user | group: worldCerealPublic
+    ('ba621c03-bc65-4669-8df9-fc621143a99f', '2dbc2120-b826-4649-939b-fff5a4a01866'),    
+    -- user: iiasa_user | group: worldCerealUser
+    ('441fb13d-abb6-4775-a732-907e5e5f0e58', '2597df23-94d9-41e0-91f3-7ea633ae27f2'),
+    -- user: iiasa_user | group: worldCerealPublic
+    ('441fb13d-abb6-4775-a732-907e5e5f0e58', '2dbc2120-b826-4649-939b-fff5a4a01866'),
+    -- user: cs_user | group: worldCerealUser
+    ('3c8f1575-3a32-497e-9a77-0a75f0bcf86d', '2597df23-94d9-41e0-91f3-7ea633ae27f2'),
+    -- user: cs_user | group: worldCerealPublic
+    ('3c8f1575-3a32-497e-9a77-0a75f0bcf86d', '2dbc2120-b826-4649-939b-fff5a4a01866'),    
+    -- user: esa_user | group: worldCerealUser
+    ('cbbf5f25-684d-4236-bd33-613f21a16dac', '2597df23-94d9-41e0-91f3-7ea633ae27f2'),
+    -- user: esa_user | group: worldCerealPublic
+    ('cbbf5f25-684d-4236-bd33-613f21a16dac', '2dbc2120-b826-4649-939b-fff5a4a01866'),
+    -- user: cs_admin | group: worldCerealUser
+    ('5db9613d-f8da-4167-bde6-11f9d84df775', '2597df23-94d9-41e0-91f3-7ea633ae27f2'),
+    -- user: cs_admin | group: worldCerealPublic
+    ('5db9613d-f8da-4167-bde6-11f9d84df775', '2dbc2120-b826-4649-939b-fff5a4a01866'),
+    -- user: demo_rdm | group: worldCerealUser
+    ('8c676118-a49e-4817-8b3f-edc742fdf43c', '2597df23-94d9-41e0-91f3-7ea633ae27f2'),
+    -- user: demo_rdm | group: worldCerealPublic
+    ('8c676118-a49e-4817-8b3f-edc742fdf43c', '2dbc2120-b826-4649-939b-fff5a4a01866'),
+    -- user: ewoc_internal | group: worldCerealUser
+    ('a7365eb7-e986-4660-8eed-3dd3a6350024', '2597df23-94d9-41e0-91f3-7ea633ae27f2'),
+    -- user: ewoc_internal | group: worldCerealPublic
+    ('a7365eb7-e986-4660-8eed-3dd3a6350024', '2dbc2120-b826-4649-939b-fff5a4a01866')
 ON CONFLICT DO NOTHING;
 
 INSERT INTO "user"."permissions"
@@ -35,13 +80,20 @@ ON CONFLICT DO NOTHING;
 INSERT INTO "user"."groupPermissions"
     ("groupKey", "permissionKey")
 VALUES
+    -- group: worldCerealAdmin | null specific worldCerealProductMetadata create
     ('998c5760-e42e-457f-aa6d-247af6352c73', 'b809c988-cfff-427d-9572-1e40a2fe92fe'),
+    -- group: worldCerealAdmin | null specific worldCerealProductMetadata view
     ('998c5760-e42e-457f-aa6d-247af6352c73', 'a405e8cf-13e0-4d9a-b039-91e4f8854b07'),
+    -- group: worldCerealAdmin | null specific worldCerealProductMetadata update
     ('998c5760-e42e-457f-aa6d-247af6352c73', 'fd5aba3a-c8f8-493c-a304-a75e19c64df6'),
+    -- group: worldCerealAdmin | null specific worldCerealProductMetadata delete
     ('998c5760-e42e-457f-aa6d-247af6352c73', '647a7004-2a62-445d-a78d-43ac8b1bdb6e'),
 
+    -- group: worldCerealAdmin | null user users create
     ('998c5760-e42e-457f-aa6d-247af6352c73', '6897b1fc-a3e3-4195-a41a-f492d4a9df2a'),
 
+    -- group: worldCerealUser | null specific worldCerealProductMetadata create
     ('2597df23-94d9-41e0-91f3-7ea633ae27f2', 'b809c988-cfff-427d-9572-1e40a2fe92fe'),
+    -- group: worldCerealUser | null specific worldCerealProductMetadata update
     ('2597df23-94d9-41e0-91f3-7ea633ae27f2', 'fd5aba3a-c8f8-493c-a304-a75e19c64df6')
 ON CONFLICT DO NOTHING;
