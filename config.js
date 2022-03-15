f24e928da67da1ecacb144745f841a7772920624
Martin Babic <martin.babic@gisat.cz>
2022-03-15 08:37:06 +0100

    add configuration entries for mapserver & mapproxy

--- config.js
+++ config.js
@@ -3,6 +3,14 @@ const ptr4 = {
 	urlMapServer: "http://localhost/mapserver", // url on which MapServer is accessible
 	masterPort: 9850,
 	isBehindKong: false, // allow auto login based on X-User-Info header which is provided by KongHQ, potential security risk if used without KongHQ
+	mapserver: {
+		url: "http://localhost:8050", // url on which MapServer is accessible
+		mapsPath: "/etc/mapserver/maps",
+		storagePath: "/etc/mapserver/storage"
+	},
+	mapproxy: {
+		url: "http://localhost:8051"
+	},
 	pgConfig: {
 		normal: {
 			user: `panther`,
