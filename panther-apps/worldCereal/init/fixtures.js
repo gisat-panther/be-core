06e5d9012bd5b516555f4a8c6e88e7ab6f11764e
Martin Babic <martin.babic@gisat.cz>
2023-03-13 07:50:38 +0000

    [WorldCereal] bug fix & formatting

--- panther-apps/worldCereal/init/fixtures.js
+++ panther-apps/worldCereal/init/fixtures.js
@@ -5,36 +5,40 @@ const config = require('../../../config');
 
 function exec() {
     try {
-        if (config.projects.worldCereal.fixtures) {
+        if (
+            config.projects
+            && config.projects.worldCereal
+            && config.projects.worldCereal.fixtures
+        ) {
             setTimeout(async () => {
                 console.log("#WorldCereal# Fixtures init!");
                 try {
                     await fixturesLocal.importLocal({ file: "fixtures.sql", path: `${process.cwd()}/panther-apps/worldCereal/fixtures.sql`, user: { type: "user" } });
-                } catch(e) {
+                } catch (e) {
                     console.log("#WorldCereal# fixtures.sql |", e.message);
                 }
 
                 try {
                     await fixturesRemote.importRemote({ file: "global.geojson", url: `http://gisat-gis.eu-central-1.linodeobjects.com/worldcereal/fixtures/production/global.geojson`, user: { key: "ba621c03-bc65-4669-8df9-fc621143a99f", realKey: "ba621c03-bc65-4669-8df9-fc621143a99f", type: "user" } });
-                } catch(e) {
+                } catch (e) {
                     console.log("#WorldCereal# global.geojson |", e.message);
                 }
 
                 try {
                     await fixturesRemote.importRemote({ file: "brazil.geojson", url: `http://gisat-gis.eu-central-1.linodeobjects.com/worldcereal/fixtures/production/brazil.geojson`, user: { key: "ba621c03-bc65-4669-8df9-fc621143a99f", realKey: "ba621c03-bc65-4669-8df9-fc621143a99f", type: "user" } });
-                } catch(e) {
+                } catch (e) {
                     console.log("#WorldCereal# brazil.geojson |", e.message);
                 }
 
                 try {
                     await fixturesRemote.importRemote({ file: "ukraine.geojson", url: `http://gisat-gis.eu-central-1.linodeobjects.com/worldcereal/fixtures/production/ukraine.geojson`, user: { key: "ba621c03-bc65-4669-8df9-fc621143a99f", realKey: "ba621c03-bc65-4669-8df9-fc621143a99f", type: "user" } });
-                } catch(e) {
+                } catch (e) {
                     console.log("#WorldCereal# ukraine.geojson |", e.message);
                 }
 
                 try {
                     await fixturesRemote.importRemote({ file: "fixtures.json", url: `http://gisat-gis.eu-central-1.linodeobjects.com/worldcereal/fixtures/production/fixtures.json`, user: { key: "ba621c03-bc65-4669-8df9-fc621143a99f", realKey: "ba621c03-bc65-4669-8df9-fc621143a99f", type: "user" } });
-                } catch(e) {
+                } catch (e) {
                     console.log("#WorldCereal# fixtures.json |", e.message);
                 }
             }, 10000);
